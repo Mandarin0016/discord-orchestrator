4ac7797d5cd2d8e48d0b8ac580373121abb7ee81
viktoraleksandrov <viktor.aleksandrov@tide.co>
2023-12-16 14:46:08 +0200

    Add test coverage

--- discord-orchestrator-svc/src/test/java/orchestrator/user/service/UserServiceUTest.java
+++ discord-orchestrator-svc/src/test/java/orchestrator/user/service/UserServiceUTest.java
@@ -1,9 +1,11 @@
 package orchestrator.user.service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import orchestrator.common.model.user.UserAuthority;
 import orchestrator.common.model.user.UserRole;
 import orchestrator.user.command.input.UserRegisterInput;
-import orchestrator.user.command.output.UserProfileOutput;
+import orchestrator.user.exception.UserDomainException;
 import orchestrator.user.model.User;
 import orchestrator.user.properties.UserProperties;
 import orchestrator.user.repository.UserRepository;
@@ -11,10 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Set;
@@ -23,6 +22,7 @@ import static orchestrator.TestBuilders.aRandomUserBuilder;
 import static orchestrator.TestBuilders.aRandomUserRegisterInputBuilder;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -41,8 +41,9 @@ public class UserServiceUTest {
     @Nested
     @DisplayName("Tests for method register()")
     class Register {
+
         @Test
-        public void givenValidRegisterInput_whenRegister_thenUserSuccessfullySaved() {
+        void givenValidRegisterInput_whenRegister_thenUserSuccessfullySaved() {
 
             // given
             UserRegisterInput registerInput = aRandomUserRegisterInputBuilder().build();
@@ -50,7 +51,6 @@ public class UserServiceUTest {
             boolean accountAlreadyExist = false;
             UserRole accountRole = UserRole.USER;
             Set<UserAuthority> accountAuthorities = Set.of(UserAuthority.CREATE_DISCORD_SERVER_SETUP_REQUEST);
-
             User savedUser = aRandomUserBuilder()
                     .firstName(registerInput.getFirstName())
                     .lastName(registerInput.getLastName())
@@ -72,45 +72,69 @@ public class UserServiceUTest {
             when(userRepository.save(any(User.class)))
                     .thenReturn(savedUser);
 
-
             // when
             userService.register(registerInput);
 
             // then
             verify(userRepository, times(1))
                     .existsByUsernameOrEmail(registerInput.getUsername(), registerInput.getEmail());
-            verify(userRepository, times(1))
-                    .save(userArgumentCaptor.capture());
             verify(userProperties, times(1))
                     .getDefaultAccountState();
             verify(userProperties, times(1))
                     .getDefaultRole();
             verify(userProperties, times(1))
                     .getDefaultAuthorities();
+            verify(userRepository).save(userArgumentCaptor.capture());
 
             User userToBeSaved = userArgumentCaptor.getValue();
-            assertThat(userToBeSaved.getId(), isNull());
+            assertThat(userToBeSaved.getId(), is(nullValue()));
             assertThat(userToBeSaved.getFirstName(), is(registerInput.getFirstName()));
             assertThat(userToBeSaved.getLastName(), is(registerInput.getLastName()));
             assertThat(userToBeSaved.getEmail(), is(registerInput.getEmail()));
             assertThat(userToBeSaved.getUsername(), is(registerInput.getUsername()));
             assertThat(userToBeSaved.getPassword(), not(registerInput.getPassword()));
             assertThat(userToBeSaved.isActive(), is(accountState));
-            assertThat(userToBeSaved.getCreateOn(), isNull());
-            assertThat(userToBeSaved.getUpdatedOn(), isNull());
+            assertThat(userToBeSaved.getCreateOn(), is(nullValue()));
+            assertThat(userToBeSaved.getUpdatedOn(), is(nullValue()));
             assertThat(userToBeSaved.getRole(), is(accountRole));
             assertThat(userToBeSaved.getAuthorities(), hasSize(1));
             assertThat(userToBeSaved.getAuthorities(), is(accountAuthorities));
         }
 
         @Test
-        public void givenExistingEmail_whenRegister_thenThrowUserDomainException() {
+        void givenExistingEmailOrUsername_whenRegister_thenThrowUserDomainException() {
 
+            // given
+            UserRegisterInput registerInput = aRandomUserRegisterInputBuilder().build();
+            when(userRepository.existsByUsernameOrEmail(any(), any()))
+                    .thenReturn(true);
+
+            // when & then
+            assertThrows(UserDomainException.class, () -> userService.register(registerInput));
+            verifyNoInteractions(userProperties);
+            verify(userRepository, times(1))
+                    .existsByUsernameOrEmail(any(), any());
+            verifyNoMoreInteractions(userRepository);
         }
 
         @Test
-        public void givenExistingUsername_whenRegister_thenThrowUserDomainException() {
+        void givenInvalidMessageDigestAlgorithm_whenRegister_thenThrowUserDomainException() {
 
+            // given
+            UserRegisterInput registerInput = aRandomUserRegisterInputBuilder().build();
+            when(userRepository.existsByUsernameOrEmail(any(), any()))
+                    .thenReturn(false);
+            try (MockedStatic<MessageDigest> mocked = mockStatic(MessageDigest.class)) {
+
+                mocked.when(() -> MessageDigest.getInstance(any())).thenThrow(NoSuchAlgorithmException.class);
+
+                // when & then
+                assertThrows(UserDomainException.class, () -> userService.register(registerInput));
+                verifyNoInteractions(userProperties);
+                verify(userRepository, times(1))
+                        .existsByUsernameOrEmail(any(), any());
+                verifyNoMoreInteractions(userRepository);
+            }
         }
     }
 }
